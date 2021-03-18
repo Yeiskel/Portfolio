425709d6b0cf4011c54fc5789e1dc118af7ec76f
Yeiskel <54485688+Yeiskel@users.noreply.github.com>
2021-03-18 00:44:10 -0400

    Added game to experimetn

---
+++ game_script.js
@@ -0,0 +1,27 @@
+import Paddle from './paddle.js';
+
+let canvas = document.getElementById("gameScreen");
+let context = canvas.getContext("2d");
+
+const GAME_WIDTH = 800;
+const GAME_HEIGHT = 600;
+
+
+
+let paddle = new Paddle(GAME_WIDTH, GAME_HEIGHT);
+
+paddle.draw(context);
+
+function game_loop(timestamp) {
+
+    let deltaTime = timestamp -lastTime;
+    lastTime = timestamp;
+
+    context.clearRect(0,0,800,600);
+    paddle.update(deltaTime);
+    paddle.draw(context);
+
+    requestAnimationFrame(game_loop);
+}
+
+game_loop();
---
+++ script.js
@@ -0,0 +1,8 @@
+function dark_mode() {
+    document.body.style.backgroundColor= "black"
+        
+}
+function light_mode() {
+    document.body.style.backgroundColor= "white"
+        
+}
